426c86cd05b5c8af52ad93ad0392628dd90e340c
Pedro Ávila <pedroavila95@gmail.com>
2017-11-26 16:44:22 -0200

    Add handler for MongoDB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
-mongoose.connect(dbURI, { useMongoClient: true })
+mongoose
+  .connect(dbURI, { useMongoClient: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(e => {
+    console.error('Could not connect to MongoDB')
+    console.error(e)
+    process.exit(1)
+  })
+
 
 module.exports = { mongoose }
